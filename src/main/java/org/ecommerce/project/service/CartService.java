6e9a40d968a931c66731008ecbf1ec6ae1164136
Ani <aniruddhyagoswami6@gmail.com>
2025-09-20 15:42:24 +0530

    Add `getAllCarts` functionality to CartService and expose API endpoint in CartController

--- src/main/java/org/ecommerce/project/service/CartService.java
+++ src/main/java/org/ecommerce/project/service/CartService.java
@@ -2,6 +2,10 @@ package org.ecommerce.project.service;
 
 import org.ecommerce.project.payload.CartDTO;
 
+import java.util.List;
+
 public interface CartService {
     public CartDTO addProductToCart(Long productId, Integer quantity);
+
+    List<CartDTO> getAllCarts();
 }
